112b36655e22e38a3ea887323b6e5174b89cf75a
Печенюк Валерий <home.peva@gmail.com>
2016-11-04 11:39:48 +0200

    refactored gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,103 +1,131 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
-var cssmin = require("gulp-cssmin");
-var autoprefixer = require("gulp-autoprefixer");
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require("gulp-uglify");
-var concat = require("gulp-concat");
-var rename = require("gulp-rename");
-var plumber = require("gulp-plumber");
-var rigger = require("gulp-rigger");
+var jshint = require("gulp-jshint");
 var del = require("del");
-var gulpif = require("gulp-if");
-var runSequence = require('run-sequence');
+var runSequence = require("run-sequence");
 var browserSync = require("browser-sync").create();
-var reload = browserSync.reload;
-var argv = require('minimist')(process.argv.slice(2));
+var argv = require("minimist")(process.argv.slice(2));
+var $ = require("gulp-load-plugins")();
 
-var DEST = './build';  
 var RELEASE = !!argv.release;  
 var AUTOPREFIXER_BROWSERS = [             
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+    "ie >= 10",
+    "ie_mob >= 10",
+    "ff >= 30",
+    "chrome >= 34",
+    "safari >= 7",
+    "opera >= 23",
+    "ios >= 7",
+    "android >= 4.4",
+    "bb >= 10"
 ];
+var path = {
+    build: { 
+        html: "build/",
+        js: "build/js/",
+        styles: "build/css/",
+        fonts: "build/fonts/",
+        img: "build/img/"
+    },
+    src: { 
+        html: "src/*.html", 
+        js: "src/js/[^_]*.js",
+        styles: "src/sass/*.scss",
+        fonts: "src/fonts/**/*.*",
+        img: "src/img/**/*.*"
+    },
+    watch: { 
+        html: "src/**/*.html",
+        js: "src/js/**/*.js",
+        styles: "src/sass/**/*.scss",
+        fonts: "src/fonts/**/*.*",
+        img: "src/img/**/*.*",
+        reload: "build/**/*.*"
+    },
+    clean: "./build", 
+    dest: "./build" 
+};
 
-gulp.task("sass", function () {
-    return gulp.src("./src/sass/main.scss")
-        .pipe(plumber())
-        .pipe(gulpif(!RELEASE, sourcemaps.init()))
-        .pipe(sass())
-        .pipe(autoprefixer({
+gulp.task("sass:build", function () {
+    return gulp.src(path.src.styles)
+        .pipe($.plumber())
+        .pipe($.if(!RELEASE, $.sourcemaps.init()))
+        .pipe($.sass())
+        .pipe($.autoprefixer({
             browsers: AUTOPREFIXER_BROWSERS,
             cascade: false
         }))
-        .pipe(cssmin())
-        .pipe(rename({
+        .pipe($.if(RELEASE, $.cssmin()))
+        .pipe($.rename({
             suffix: ".min",
             extname: ".css"
         }))
-        .pipe(gulpif(!RELEASE, sourcemaps.write(".")))
-        .pipe(gulp.dest(DEST +"/css"));
+        .pipe($.if(!RELEASE, $.sourcemaps.write(".")))
+        .pipe(gulp.dest(path.build.styles));
 });
 
-gulp.task("script", function(){
-    return gulp.src("./src/js/*.js")
-        .pipe(gulpif(!RELEASE, sourcemaps.init()))
-        .pipe(rigger())
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(rename({
+gulp.task("script:build", function(){
+    return gulp.src(path.src.js)
+        .pipe($.if(!RELEASE, $.sourcemaps.init()))
+        .pipe($.rigger())
+        .pipe(jshint()) 
+        .pipe(jshint.reporter("jshint-stylish"))
+        .pipe($.concat('main.js'))
+        .pipe($.uglify())
+        .pipe($.rename({
             suffix: ".min",
             extname: ".js"
         }))
-        .pipe(gulpif(!RELEASE, sourcemaps.write(".")))
-        .pipe(gulp.dest(DEST +"/js"));
+        .pipe($.if(!RELEASE, $.sourcemaps.write(".")))
+        .pipe(gulp.dest(path.build.js));
 });
 
-gulp.task("assets", function() {
-    return gulp.src("src/**/*.html")
-        .pipe(rigger())
-        .pipe(gulp.dest(DEST))
+gulp.task("assets:build", function() {
+    return gulp.src(path.src.html)
+        .pipe($.rigger())
+        .pipe(gulp.dest(path.build.html))
 });
 
-
-gulp.task("fonts", function() {
-    return gulp.src("src/fonts/**/*.*")
-        .pipe(gulp.dest(DEST+ "/fonts"));
+gulp.task("fonts:build", function() {
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
 });
 
-
-gulp.task("clean", del.bind(null, [DEST]));
+gulp.task("clean", del.bind(null, path.clean));
 
 gulp.task("build", ["clean"], function (cb) {
-    runSequence(["sass", "script" ,"assets", "fonts"], cb);
+    runSequence(["sass:build", "script:build","assets:build", "fonts:build"], cb);
 });
 
 gulp.task("browser-sync", function () {
     browserSync.init({
         server: {
-            baseDir: DEST
+            baseDir: path.dest
         }
     });
+    browserSync.watch(path.watch.reload).on("change", browserSync.reload);
 });
 
-gulp.task("watch", function () {
-    gulp.watch("./src/sass/*.sass", ["sass"]);
-    gulp.watch("./src/js/*.js", ["script"]);
-    gulp.watch("./src/**/*.html",["assets"]);
-    gulp.watch("./src/fonts/**/*.*",["fonts"]);
-    gulp.watch( DEST + '/**/*.*').on('change', reload);
+gulp.task("watch", function(){
+    $.watch([path.watch.html], function(event, cb) {
+        gulp.start("assets:build");
+    });
+  
+    $.watch([path.watch.styles], function(event, cb) {
+        gulp.start("sass:build");
+    });
+
+    $.watch([path.watch.js], function(event, cb) {
+        gulp.start("script:build");
+    });
+
+    $.watch([path.watch.fonts], function(event, cb) {
+        gulp.start("fonts:build");
+    });
+
 });
 
 gulp.task("serve", function (cb) {
-    runSequence("build",["browser-sync", "watch"] , cb);
+    runSequence("build", ["browser-sync","watch"], cb);
 });
 
 gulp.task("default", ["serve"]);
